16064086ff607de705781904cce4188e07b82e59
nboaram <adrian@nboaram.co.uk>
2019-06-25 15:31:23 +0000

    install and uninstall commands

---
+++ jenkinsinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-8--jdk
+sudo useradd --create-home jenkins
+sudo usermod --shell /bin/bash jenkins
+sudo usermod gid sudo
+wget http://updates.jenkins-ci.org/latest/jenkins.war
+chmod 777 jenkins.war
+sudo mv jenkins.war /home/jenkins/
+sudo cp jenkins.service /etc/systemd/system/jenkins.service
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+
+
+
---
+++ jenkinsuninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo systemctl stop jenkins
+sudo apt-get remove --purge jenkins
+sudo userdel jenkins
+sudo rm -r /home/jenkins
+sudo rm /etc/systemd/system/jenkins.service
+
