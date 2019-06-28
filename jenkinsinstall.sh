7925859764b7f8fdd8bb1a265034a43a00240727
Ubuntu <adrian@hostname.ctgmyfbjxx2uvep55ctqy1drag.zx.internal.cloudapp.net>
2019-06-28 13:27:56 +0000

    print statements

--- jenkinsinstall.sh
+++ jenkinsinstall.sh
@@ -5,10 +5,15 @@ sudo useradd --create-home jenkins
 sudo usermod --shell /bin/bash jenkins
 sudo usermod gid sudo
 wget http://updates.jenkins-ci.org/latest/jenkins.war
+echo "Modding"
 chmod 777 jenkins.war
+echo "Moving Jenkins.war"
 sudo mv jenkins.war /home/jenkins/
+echo "Moving Jenkins.Service"
 sudo cp jenkins.service /etc/systemd/system/jenkins.service
+echo "system reload"
 sudo systemctl daemon-reload
+echo "start jenkins"
 sudo systemctl start jenkins
 
 
